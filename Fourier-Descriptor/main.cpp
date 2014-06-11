2beff066bda0c41f16716299f4346d098983d143
kkcocogogo <kkcocogogo@gmail.com>
2014-06-11 19:07:50 +0800

    Initial Commit

---
+++ Fourier-Descriptor/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Fourier-Descriptor
+//
+//  Created by Tim Feirg on 6/11/14.
+//  Copyright (c) 2014 SYSU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
