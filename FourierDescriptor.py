a68d1fabe3350d08e5765ba17acde699a34432bf
kkcocogogo <kkcocogogo@gmail.com>
2014-06-16 14:30:50 +0800

    almost finish the first one

--- FourierDescriptor.py
+++ FourierDescriptor.py
@@ -2,7 +2,7 @@
 
 import cv2
 import numpy
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 
 def findDescriptor(img):
@@ -13,24 +13,31 @@ def findDescriptor(img):
         cv2.CHAIN_APPROX_SIMPLE,
         contour)
     contour_array = contour[0][:, 0, :]
-    contour_complex = contour_array[:, 0]
-    contour_complex += 1j*contour_array[:, 1]
-    # time to conduct some fourier transform
+    contour_complex = numpy.empty(contour_array.shape[:-1], dtype=complex)
+    contour_complex.real = contour_array[:, 0]
+    contour_complex.imag = contour_array[:, 1]
     fourier_result = numpy.fft.fft(contour_complex)
-    return fourier_result
+    magnitude = numpy.absolute(fourier_result)
+    return fourier_result, contour
 
 
 def reconstruct(dscptr, degree):
     """ reconstruct(dscptr, degree) attempts to reconstruct the image
     using the first [degree] descriptors of dscptr"""
+    dscptr = numpy.fft.fftshift(dscptr)
+    plt.subplot(211)
+    plt.plot(numpy.absolute(dscptr))
+    center_index = len(dscptr)/2
+    print(center_index)
+    descriptor_in_use = dscptr[center_index - degree:center_index + degree]
+    plt.subplot(212)
+    plt.plot(numpy.absolute(descriptor_in_use))
+    plt.show()
+    contour_reconstruct = numpy.fft.ifft(descriptor_in_use)
+    pass
 
-
-cv2.destroyAllWindows()
-
-# create a circle for testing
+black = numpy.zeros((500, 500), numpy.uint8)
 star = cv2.imread("/Users/timfeirg/Documents/Fourier-Descriptor/star.jpg", 0)
 retval, star = cv2.threshold(star, 127, 255, cv2.THRESH_BINARY)
-fourier_result = findDescriptor(star)
-
-# reconstruct using certain amount of descriptors
-reconstruct(fourier_result, 3)
+fourier_result, contour = findDescriptor(star)
+contour_reconstruct = reconstruct(fourier_result, 50)
